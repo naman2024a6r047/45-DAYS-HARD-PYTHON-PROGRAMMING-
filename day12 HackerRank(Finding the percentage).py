38ab6c2d7b3dc723b5c7fd1c8aa6ff378a4f9505
Naman Sen <naman0503o@gmail.com>
2026-07-07 12:31:03 +0530

    Create day12 HackerRank(Finding the percentage).py

---
+++ day12 HackerRank(Finding the percentage).py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    for name,score in student_marks.items():
+        if name == query_name:
+            n=len(score)
+            result=sum(score)/n
+            print(f"{result:.2f}")

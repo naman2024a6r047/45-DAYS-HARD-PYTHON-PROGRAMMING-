39a65e8ae0d1dda5a8e5c70b17c06c33240d405a
Naman Sen <naman0503o@gmail.com>
2026-07-12 13:27:35 +0530

    Create day19(e)HackerRank(String Validation).py

---
+++ day19(e)HackerRank(String Validation).py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    s = input()
+    print(any(ch.isalnum() for ch in s))
+    print(any(ch.isalpha() for ch in s))
+    print(any(ch.isdigit() for ch in s))
+    print(any(ch.islower() for ch in s))
+    print(any(ch.isupper() for ch in s))
+
+# any([True, True, False])
+# Since there is at least one True, the result is:
+# True
